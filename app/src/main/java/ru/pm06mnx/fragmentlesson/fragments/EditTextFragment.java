cf112155be405c785f64a45243e67305fb8ae4ee
pm06mnx <V521@yandex.ru>
2019-02-12 17:30:28 +0300

    Обработка смены конфигурации

--- app/src/main/java/ru/pm06mnx/fragmentlesson/fragments/EditTextFragment.java
+++ app/src/main/java/ru/pm06mnx/fragmentlesson/fragments/EditTextFragment.java
@@ -1,6 +1,5 @@
 package ru.pm06mnx.fragmentlesson.fragments;
 
-import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -15,12 +14,22 @@ import ru.pm06mnx.fragmentlesson.R;
 
 public class EditTextFragment extends Fragment {
 
+    private static final String TEXTS = "TEXTS";
     private EditText mEditText;
     private ArrayList<String> mTexts = new ArrayList<>();
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //setRetainInstance(true);
+    }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        if (savedInstanceState != null) {
+            mTexts = savedInstanceState.getStringArrayList(TEXTS);
+        }
         return inflater.inflate(R.layout.edit_text_fragment_layout, container, false);
     }
 
@@ -28,6 +37,12 @@ public class EditTextFragment extends Fragment {
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         mEditText = view.findViewById(R.id.edit_text);
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        outState.putStringArrayList(TEXTS, mTexts);
     }
 
     public void addChars(String chars) {
